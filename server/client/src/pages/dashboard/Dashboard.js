1e9fcf68ecbaf2863ec59089298e339296121f79
cogicgoran <cogicgoran99@gmail.com>
2022-01-07 14:35:53 +0100

    change bracket operator to dot operator in dashboard className

--- server/client/src/pages/dashboard/Dashboard.js
+++ server/client/src/pages/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import styles from './Dashboard.module.css';
 
 function Dashboard() {
     return (
-        <div className={styles['dashboard']}>
+        <div className={styles.dashboard}>
             <DashboardRecentReceipts/>
             <DashboardCurrentMonth/>
             <DashboardCategories/>
